2aa8e2715820ea08dc98af530eaa514bdb82c7d7
andresmauricio <andresacelas@outlook.com>
2020-04-15 19:37:32 -0500

    pipes percent, number, uppercase, lowercas, slice, and currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  public name: string = "Andrés Mauricio";
+  public array: number[] = [1,2,3,4,5,6,7,8,9];
+  public PI = Math.PI;
+  public percet: number = 0.234;
+  public money: number = 989365;
 }
